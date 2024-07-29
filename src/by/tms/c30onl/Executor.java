80c6488464d2efe70bbd07691f2a0272f35cdea0
alex garag <alex.garag@bk.ru>
2024-07-29 14:44:41 +0300

    реализация на откровенно подсмотренном :(

---
+++ src/by/tms/c30onl/Executor.java
@@ -0,0 +1,28 @@
+package by.tms.c30onl;
+
+import by.tms.c30onl.lesson16.OwnArrayList;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class Executor {
+    public static void main(String[] args) {
+        OwnArrayList<String> array = new OwnArrayList(1);
+
+        System.out.println(array.size());
+        System.out.println(array.add("String0"));
+        System.out.println(array.get(0));
+        System.out.println(array.size());
+        System.out.println(array.add("String1"));
+
+//                List<Integer> array1 = new ArrayList<Integer>();
+//        array.add(1);
+//        array.add(2);
+//        array.add(3);
+//        array.add(4);
+//        array.remove(2);
+
+
+        int i = 0;
+    }
+}
---
+++ src/by/tms/c30onl/lesson16/OwnArrayList.java
@@ -0,0 +1,122 @@
+package by.tms.c30onl.lesson16;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class OwnArrayList<E> implements Collection<E> {
+    private int size;
+    private static final int DEFAULT_CAPACITY = 16;
+    private int capacity;
+    private static final Object[] EMPTY_ELEMENT_DATA = {};
+    private static final Object[] DEFAULT_CAPACITY_EMPTY_ELEMENT_DATA = {};
+    private Object[] arrayData;
+
+    public OwnArrayList(int requestedCapacity) {
+        if (requestedCapacity > 0) {
+            arrayData = new Object[requestedCapacity];
+        } else if (requestedCapacity == 0) {
+            arrayData = EMPTY_ELEMENT_DATA;
+        } else {
+            throw new IllegalArgumentException("Illegal Capacity: " + requestedCapacity);
+        }
+        capacity = requestedCapacity;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (size == capacity) {
+            capacity = capacity * 2;
+            Object[] newArray = new Object[capacity];
+            for (int i = 0; i < size; i++) {
+                newArray[i] = arrayData[i];
+                arrayData[i] = null;
+            }
+            arrayData = newArray;
+        }
+        int i = 0;
+        arrayData[size++] = e;
+        return true;
+    }
+
+//    private void expand() {
+//        int ii = capacity >> 1;
+//        capacity = capacity + ii; //capacity >> 1;
+//        Object[] newArray = new Object[capacity];
+//        for (int i = 0; i < size; i++) {
+//            newArray[i] = arrayData[i];
+//            arrayData[i] = null;
+//        }
+//        arrayData = newArray;
+//        int i = 0;
+//    }
+
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        return (E) arrayData[index];
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+}
